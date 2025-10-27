ca3b28a824eaaf74a8de139494ac17b626802c78
ArmenVv <armenvardanian2024@gmail.com>
2025-10-27 14:09:44 +0400

    Add files via upload

---
+++ stackqueuemerge.cpp
@@ -0,0 +1,20 @@
+﻿#include <queue>
+#include <stack>
+using namespace std;
+
+queue<int> mergeQueueStack(queue<int>& q, stack<int>& s) {
+    queue<int> result;
+
+
+    while (!q.empty()) {
+        result.push(q.front());
+        q.pop();
+    }
+
+    while (!s.empty()) {
+        result.push(s.top());
+        s.pop();
+    }
+
+    return result;
+}
