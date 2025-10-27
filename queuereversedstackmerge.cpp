a247396c5e1bf5cff57b74bed37381d38abee40d
ArmenVv <armenvardanian2024@gmail.com>
2025-10-27 14:10:39 +0400

    Add files via upload

---
+++ queuereversedstackmerge.cpp
@@ -0,0 +1,28 @@
+﻿#include <queue>
+#include <stack>
+using namespace std;
+
+queue<int> mergeQueueReversedStack(queue<int>& q, stack<int>& s) {
+    queue<int> result;
+    stack<int> temp;
+
+   
+    while (!q.empty()) {
+        result.push(q.front());
+        q.pop();
+    }
+
+   
+    while (!s.empty()) {
+        temp.push(s.top());
+        s.pop();
+    }
+
+    
+    while (!temp.empty()) {
+        result.push(temp.top());
+        temp.pop();
+    }
+
+    return result;
+}
